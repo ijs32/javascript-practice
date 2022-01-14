0b65cfc0c55960c909a9d7515e704e5cf0e86309
Ian S <iansilber32@gmail.com>
2022-01-14 11:07:36 -0500

    more deliberate practice

--- leetcode-and-algos/extraDeliberatePractice.js
+++ leetcode-and-algos/extraDeliberatePractice.js
@@ -47,6 +47,37 @@ function firstLetter(array) {
 
 console.log(firstLetter(input4));
 
+// Write a function that accepts an array of numbers and returns an array with each number converted into a string.
+// Example:
+let input5 = [1, 2, 3];
+// Output: ["1", "2", "3"]
+
+function convertToString(array) {
+  return array.map(function (num) {
+    return String(num);
+  });
+}
+
+console.log(convertToString(input5));
+
+// Write a function that accepts an array of numbers and returns a new array with only the even numbers.
+// Example:
+let input6 = [2, 4, 5, 1, 8, 9, 7];
+// Output: [2, 4, 8]
+
+function onlyEvens(arr) {
+  return arr.map(function (num) {
+    if (num % 2 === 0) {
+      return num;
+    }
+  });
+}
+
+console.log(onlyEvens(input6));
+// get the fibonacci number that corresponds to the number input
+//  input = 1 / 2 / 3 / 4
+// output = 0 / 1 / 1 / 2
+
 function fibonacciNumbers(n) {
   let fibonacciNumbers = [0, 1];
   let i = 1;
